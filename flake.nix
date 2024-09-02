d49d5e8c310ed6400408f0c8af5cd400fea99980
Fede Barcelona <fede_rico_94@hotmail.com>
2024-09-02 12:32:38 +0200

    build: cross-compile image to linux from mac

--- flake.nix
+++ flake.nix
@@ -5,40 +5,44 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    poetry2nix-flake,
-    utils,
-  }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      poetry2nix-flake,
+      utils,
+    }:
     utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pythonVersion = pkgs.python3; # <- Your python version here, in case you want a different one. For example, one of: python3, python310, python311, python312, etc
 
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
-          overlays = [poetry2nix-flake.overlays.default];
+          overlays = [ poetry2nix-flake.overlays.default ];
+          crossSystem = "x86_64-linux";
         };
 
-        helm_with_plugins = with pkgs;
+        helm_with_plugins =
+          with pkgs;
           wrapHelm kubernetes-helm {
             # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=kubernetes-helmPlugins
             plugins = with kubernetes-helmPlugins; [
               helm-diff # Required for `helm diff` and `helmfile apply`
             ];
           };
-        helmfile_with_plugins = pkgs.helmfile-wrapped.override {
-          inherit (helm_with_plugins) pluginsDir;
-        };
+        helmfile_with_plugins = pkgs.helmfile-wrapped.override { inherit (helm_with_plugins) pluginsDir; };
 
-        pipelines_image = pkgs.callPackage ./pipelines_image.nix {};
-      in {
+        pipelines_image = pkgs.callPackage ./pipelines_image.nix { };
+      in
+      {
         packages = {
-          miner = pkgs.callPackage ./miner_image.nix {};
+          miner = pkgs.callPackage ./miner_image.nix { };
           default = pipelines_image;
         };
-        devShells.default = with pkgs;
+        devShells.default =
+          with pkgs;
           mkShellNoCC {
             packages = [
               (poetry2nix.mkPoetryEnv {
