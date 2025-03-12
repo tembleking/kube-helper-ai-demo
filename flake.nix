7c20e76fe34d3a38c3d20dad27b2b10339157354
Fede Barcelona <fede_rico_94@hotmail.com>
2025-03-12 10:14:15 +0100

    fix: make flake work in macos

--- flake.nix
+++ flake.nix
@@ -11,54 +11,72 @@
       flake-utils,
     }:
     let
+      overlays.default = final: pkgs: rec {
+        helm-with-plugins = pkgs.wrapHelm pkgs.kubernetes-helm {
+          # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=kubernetes-helmPlugins
+          plugins = with pkgs.kubernetes-helmPlugins; [
+            helm-diff
+          ];
+        };
+
+        helmfile-with-plugins = pkgs.helmfile-wrapped.override { inherit (helm-with-plugins) pluginsDir; };
+
+        gdk = pkgs.google-cloud-sdk.withExtraComponents (
+          with pkgs.google-cloud-sdk.components;
+          [
+            gke-gcloud-auth-plugin
+          ]
+        );
+
+        pipelines-image = pkgs.callPackage ./pipelines_image.nix { };
+      };
+
       flake = flake-utils.lib.eachDefaultSystem (
         system:
         let
           pkgs = import nixpkgs {
             inherit system;
             config.allowUnfree = true;
-            crossSystem = "x86_64-linux";
+            overlays = [ self.overlays.default ];
           };
 
-          helm_with_plugins =
-            with pkgs;
-            wrapHelm kubernetes-helm {
-              # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=kubernetes-helmPlugins
-              plugins = with kubernetes-helmPlugins; [
-                helm-diff # Required for `helm diff` and `helmfile apply`
-              ];
-            };
-          helmfile_with_plugins = pkgs.helmfile-wrapped.override { inherit (helm_with_plugins) pluginsDir; };
-
-          pipelines_image = pkgs.callPackage ./pipelines_image.nix { };
+          linuxPkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [ self.overlays.default ];
+            crossSystem = "x86_64-linux";
+          };
         in
         {
           packages = {
-            default = pipelines_image;
+            default = linuxPkgs.pipelines-image;
           };
 
           apps = {
             deploy = flake-utils.lib.mkApp { drv = pkgs.callPackage ./deploy.nix { }; };
           };
 
-          devShells.default = pkgs.mkShellNoCC {
-            packages = [
-              (pkgs.python3.withPackages (
-                ps: with ps; [
-                  jedi-language-server
-                  python-lsp-server
-                ]
-              ))
-              pkgs.ruff
-              helm_with_plugins
-              helmfile_with_plugins
-            ];
-          };
+          devShells.default =
+            with pkgs;
+            mkShellNoCC {
+              packages = [
+                (python3.withPackages (
+                  ps: with ps; [
+                    jedi-language-server
+                    python-lsp-server
+                  ]
+                ))
+                ruff
+                helm-with-plugins
+                helmfile-with-plugins
+                gdk
+              ];
+            };
 
-          formatter = pkgs.nixpkgs-rfc-style;
+          formatter = pkgs.nixfmt-rfc-style;
         }
       );
 
     in
-    flake;
+    flake // { inherit overlays; };
 }
